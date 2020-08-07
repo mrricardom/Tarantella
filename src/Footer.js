4e06d127d44ba3644348caf11c38d99d4fb73422
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2020-08-07 13:06:15 -0400

    added padding to header in mobil screen

--- src/Footer.js
+++ src/Footer.js
@@ -7,8 +7,9 @@ function Footer() {
     <footer>
       <p>
         Ricardo Martini &copy; 2020, Tarantella was inspired by Apple. Icons
-        were taken from the 'ionicons' by benjsperry on icon-icons.com. Special
-        thanks to Ali Spittel and Soleil Solomon for coding advice.
+        were taken from the 'ionicons' by benjsperry on icon-icons.com. General
+        song image thanks to Juli Clover on macrumors.com. Special thanks to Ali
+        Spittel and Soleil Solomon for coding advice.
       </p>
       <div>
         <a href="https://www.linkedin.com/in/ricardo-martini-096346117/">
